67fe41d08fceb7c5fa3ef89c2d64d551e523f1c5
hnvn <hunghd.yb@gmail.com>
2022-01-04 23:33:00 +0700

    add open in safari option on ios

---
+++ packages/share_plus/share_plus/ios/Classes/FLTOpenInSafariActivity.h
@@ -0,0 +1,12 @@
+//
+//  FLTOpenInSafariActivity.h
+//  share_plus
+//
+//  Created by Ha Hung on 04/01/2022.
+//
+
+#ifndef FLTOpenInSafariActivity_h
+#define FLTOpenInSafariActivity_h
+
+
+#endif /* FLTOpenInSafariActivity_h */
---
+++ packages/share_plus/share_plus/ios/Classes/FLTOpenInSafariActivity.m
@@ -0,0 +1,8 @@
+//
+//  FLTOpenInSafariActivity.m
+//  share_plus
+//
+//  Created by Ha Hung on 04/01/2022.
+//
+
+#import <Foundation/Foundation.h>
